46aa648530935636b63fa4bdfbab58978b756476
İhsan Çungay <32899901+cungay@users.noreply.github.com>
2022-06-03 01:12:12 +0300

    Create IEventBusSubscriptionManager.cs

---
+++ PhoneBook/src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
@@ -0,0 +1,29 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBusSubscriptionManager
+    {
+        event EventHandler<string> OnRemoved;
+
+        bool IsEmpty { get; }
+
+        bool HasSubscriptionsForEvent<T>() where T : IntegrationEvent;
+
+        bool HasSubscriptionsForEvent(string eventName);
+
+        string GetEventKey<T>();
+
+        Type GetEventTypeByName(string eventName);
+
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
+
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
+
+        void AddSubscription<T, H>() where T : IntegrationEvent where H : IIntegrationEventHandler<T>;
+
+        void RemoveSubscription<T, H>() where H : IIntegrationEventHandler<T> where T : IntegrationEvent;
+
+        void Clear();
+    }
+}
