0e3f9247db899353e5a457def576340422649050
İhsan Çungay <32899901+cungay@users.noreply.github.com>
2022-06-03 01:12:05 +0300

    Create IIntegrationEventHandler.cs

---
+++ PhoneBook/src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IIntegrationEventHandler<TEvent> : IntegrationEventHandler where TEvent : IntegrationEvent
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IntegrationEventHandler
+    {
+
+    }
+}
