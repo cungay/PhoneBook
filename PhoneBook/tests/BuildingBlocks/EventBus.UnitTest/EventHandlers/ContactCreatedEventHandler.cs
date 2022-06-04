1f592d17af1274dedb5ecc7eeef96b67f5c7439a
İhsan Çungay <32899901+cungay@users.noreply.github.com>
2022-06-05 02:31:29 +0300

    Create ContactCreatedEventHandler.cs

---
+++ PhoneBook/tests/BuildingBlocks/EventBus.UnitTest/EventHandlers/ContactCreatedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using EventBus.Base.Abstraction;
+using EventBus.UnitTest.Events;
+
+namespace EventBus.UnitTest.EventHandlers
+{
+    public class ContactCreatedEventHandler : IIntegrationEventHandler<ContactCreatedEvent>
+    {
+        public Task Handle(ContactCreatedEvent @event)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
