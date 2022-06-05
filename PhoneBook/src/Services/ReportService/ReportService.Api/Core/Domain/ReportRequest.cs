9491393f9a96b25a7aca90cdb17174176f090491
İhsan Çungay <32899901+cungay@users.noreply.github.com>
2022-06-05 04:57:09 +0300

    Create ReportRequest.cs

---
+++ PhoneBook/src/Services/ReportService/ReportService.Api/Core/Domain/ReportRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace ReportService.Api.Core.Domain
+{
+    public class ReportRequest
+    {
+        /// <summary>
+        /// Gets or sets the entity identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the request date
+        /// </summary>
+        public DateTime RequestDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the report status identifier
+        /// </summary>
+        public int ReportStatusId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the report status
+        /// </summary>
+        public ReportStatus ReportStatus
+        {
+            get => (ReportStatus)ReportStatusId;
+            set => ReportStatusId = (int)value;
+        }
+    }
+}
