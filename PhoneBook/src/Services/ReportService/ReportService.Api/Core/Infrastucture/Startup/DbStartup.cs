3e76ab6e4db6408990d6a780f950e9d969c62f7c
İhsan Çungay <32899901+cungay@users.noreply.github.com>
2022-06-07 16:17:52 +0300

    Create DbStartup.cs

---
+++ PhoneBook/src/Services/ReportService/ReportService.Api/Core/Infrastucture/Startup/DbStartup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportService.Api.Core.Infrastucture.Context;
+using System.Reflection;
+
+namespace ReportService.Api.Core.Infrastucture.Startup
+{
+    public class DbStartup : IStartup
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<ReportContext>(options =>
+                {
+                    options.UseSqlServer(configuration["ConnectionString"],
+                        sqlServerOptionsAction: sqlOptions =>
+                        {
+                            sqlOptions.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+                            sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                        });
+                });
+        }
+
+        public void Configure(IApplicationBuilder application)
+        {
+            
+        }
+    }
+}
