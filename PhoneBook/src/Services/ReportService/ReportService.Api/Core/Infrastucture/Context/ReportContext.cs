ef2c668098dae0bff74c18d9ff36872f6c77e79c
İhsan Çungay <32899901+cungay@users.noreply.github.com>
2022-06-07 16:18:00 +0300

    Create ReportContext.cs

---
+++ PhoneBook/src/Services/ReportService/ReportService.Api/Core/Infrastucture/Context/ReportContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportService.Api.Core.Domain;
+using ReportService.Api.Core.Infrastucture.Mapping.Builders;
+
+namespace ReportService.Api.Core.Infrastucture.Context
+{
+    public class ReportContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "report";
+
+        public ReportContext(DbContextOptions<ReportContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ReportRequest> ReportRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ReportRequestBuilder());
+        }
+    }
+}
