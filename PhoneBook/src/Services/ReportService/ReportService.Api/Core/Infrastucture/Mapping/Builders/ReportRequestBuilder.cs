9a316cc3bd4bc3f327a9f092dbbb013a5363dc7a
İhsan Çungay <32899901+cungay@users.noreply.github.com>
2022-06-07 16:18:07 +0300

    Create ReportRequestBuilder.cs

---
+++ PhoneBook/src/Services/ReportService/ReportService.Api/Core/Infrastucture/Mapping/Builders/ReportRequestBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReportService.Api.Core.Domain;
+using ReportService.Api.Core.Infrastucture.Context;
+
+namespace ReportService.Api.Core.Infrastucture.Mapping.Builders
+{
+    public class ReportRequestBuilder : IEntityTypeConfiguration<ReportRequest>
+    {
+        public void Configure(EntityTypeBuilder<ReportRequest> builder)
+        {
+            builder.ToTable("ReportRequest", ReportContext.DEFAULT_SCHEMA);
+
+            builder.HasKey(ci => ci.Id);
+
+            builder.Property(ci => ci.Id)
+                .UseHiLo("report_request_hero")
+                .IsRequired();
+
+            builder.Property(p => p.RequestDate)
+                .IsRequired();
+
+            builder.Property(p => p.ReportStatus)
+                .IsRequired();
+        }
+    }
+}
