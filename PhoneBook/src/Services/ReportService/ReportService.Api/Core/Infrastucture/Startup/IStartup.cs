17439903c4d02ef78dd06ac6157ec1bc0d3f8ac2
İhsan Çungay <32899901+cungay@users.noreply.github.com>
2022-06-07 16:17:55 +0300

    Create IStartup.cs

---
+++ PhoneBook/src/Services/ReportService/ReportService.Api/Core/Infrastucture/Startup/IStartup.cs
@@ -0,0 +1,21 @@
+﻿namespace ReportService.Api.Core.Infrastucture.Startup
+{
+    /// <summary>
+    /// Represents object for the configuring services and middleware on application startup
+    /// </summary>
+    public interface IStartup
+    {
+        /// <summary>
+        /// Add and configure any of the middleware
+        /// </summary>
+        /// <param name="services">Collection of service descriptors</param>
+        /// <param name="configuration">Configuration of the application</param>
+        void ConfigureServices(IServiceCollection services, IConfiguration configuration);
+
+        /// <summary>
+        /// Configure the using of added middleware
+        /// </summary>
+        /// <param name="application">Builder for configuring an application's request pipeline</param>
+        void Configure(IApplicationBuilder application);
+    }
+}
